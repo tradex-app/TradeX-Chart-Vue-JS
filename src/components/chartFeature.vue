2b26852a473a17704ee840b233954da7622daaa0
leoplaw <leo@guildmedia.net>
2024-12-25 20:53:25 +0100

    features styling

--- src/components/chartFeature.vue
+++ src/components/chartFeature.vue
@@ -11,9 +11,23 @@
   </div>
 </template>
 
-<style scoped>
+<style>
   .chartFeature {
     border: 1px solid #666;
     border-radius: .5em;
+  }
+
+  button,
+  select {
+    color: #ccc;
+    border-radius: 8px;
+    border: 1px solid transparent;
+    padding: 0.6em 1.2em;
+    font-size: 1em;
+    font-weight: 500;
+    font-family: inherit;
+    background-color: #1a1a1a;
+    cursor: pointer;
+    transition: border-color 0.25s;
   }
 </style>
