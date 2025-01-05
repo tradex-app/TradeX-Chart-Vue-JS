70e6ab0dbf001b364f63486405838b060bede3a9
leoplaw <leo@guildmedia.net>
2025-01-05 15:47:44 +0100

    styling

--- src/components/chartFeature.vue
+++ src/components/chartFeature.vue
@@ -15,6 +15,7 @@
   .chartFeature {
     border: 1px solid #666;
     border-radius: .5em;
+    padding: .25em;
   }
 
   button,
@@ -22,11 +23,12 @@
     color: #ccc;
     border-radius: 8px;
     border: 1px solid transparent;
-    padding: 0.6em 1.2em;
+    padding: 0.4em .8em;
+    margin-right: 0.5em;
     font-size: 1em;
     font-weight: 500;
     font-family: inherit;
-    background-color: #1a1a1a;
+    background-color: #222;
     cursor: pointer;
     transition: border-color 0.25s;
   }
